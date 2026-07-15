8915aa4f5b930da393b986c58f1f65b25b679f3b
Alexandre Lyra <alexlyra@gmail.com>
2026-07-15 00:54:03 -0300

    fix(utils): add formatCPF and formatCurrency to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -43,3 +43,20 @@ export function safeLocalStorageSet(key: string, value: string) {
     }
   }
 }
+
+export const formatCurrency = (value: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value);
+};
+
+export const formatCPF = (value: string) => {
+  if (!value) return '';
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
