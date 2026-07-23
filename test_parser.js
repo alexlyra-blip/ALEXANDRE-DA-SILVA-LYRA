ae193cd827130e6b3f7a9ac62dd8eb5da96dffe2
Alexandre Lyra <alexlyra@gmail.com>
2026-07-23 20:05:41 -0300

    chore: remove test files

--- test_parser.js
+++
@@ -1,4 +0,0 @@
-const { parseConsultaResponse } = require('./lib/multicorban');
-const payload = require('./payload_utf8.json');
-const res = parseConsultaResponse(payload, true);
-console.log('CardLoansList:', JSON.stringify(res[0].CardLoansList, null, 2));
